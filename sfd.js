82e218f7c048989a1fea7655c0c8feb1cbb0cac5
duletashev1 <116757605+duletashev1@users.noreply.github.com>
2023-07-22 02:19:27 +0200

    Add files via upload

---
+++ sfd.js
@@ -0,0 +1,9 @@
+let hideMeny=document.querySelector(".fa");
+let navLinks=document.getElementById("navLinks");
+let menu;
+function showMenu(){
+    navLinks.style.right="0";
+}
+function hideMenu(){
+    navLinks.style.right="-200px"
+}
